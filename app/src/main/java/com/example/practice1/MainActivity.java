9fa86615c9a86eca95754e78e2f667f0ce0e2344
Rucha <bhopale.rucha@gmail.com>
2024-02-01 16:58:52 +0530

    initial commit

---
+++ app/src/main/java/com/example/practice1/MainActivity.java
@@ -0,0 +1,70 @@
+package com.example.practice1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.KeyEvent;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+    private TextView txtInfo;
+    private EditText edtInfo;
+    private Button btnSetInfo;
+    private LinearLayout linearLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // setContentView(R.layout.activity_main);
+        linearLayout = new LinearLayout(this);
+        linearLayout.setOrientation(LinearLayout.VERTICAL);
+        linearLayout.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.MATCH_PARENT));
+        //linearLayout.setGravity(Gravity.CENTER_VERTICAL Gravity.CENTER_HORIZONTAL);
+
+        linearLayout.setGravity(Gravity.CENTER);
+
+        txtInfo = new TextView(this);
+        txtInfo.setText("Welcome to Android..");
+        txtInfo.setTextSize(50);
+        txtInfo.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+        linearLayout.addView(edtInfo);
+
+        btnSetInfo = new Button(this);
+        btnSetInfo.setText("Set Info");
+        btnSetInfo.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+        linearLayout.addView(btnSetInfo);
+        setContentView(linearLayout);
+        btnSetInfo.setOnClickListener(new BtnClickListener());
+
+    }
+
+class BtnClickListener implements View.OnClickListener{
+    @Override
+    public void onClick(View v) {
+        txtInfo.setText(edtInfo.getText().toString());
+    }
+}
+class BtnTouchListener implements View.OnTouchListener
+{
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        txtInfo.setText((edtInfo.getText().toString()));
+        return false;
+    }
+}
+    class BtnKeyListener implements View.OnKeyListener{
+        @Override
+        public boolean onKey(View v, int keyCode, KeyEvent event) {
+            txtInfo.setText(edtInfo.getText().toString());
+            return false;
+        }
+    }
+}
