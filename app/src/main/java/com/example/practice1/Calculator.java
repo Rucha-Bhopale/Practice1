ce05ee958a484e19262e2d81eb4e31c3f1c31659
Rucha-Bhopale <bhopale.rucha@gmail.com>
2024-02-06 10:20:51 +0530

    UI and development Completed

--- app/src/main/java/com/example/practice1/Calculator.java
+++ app/src/main/java/com/example/practice1/Calculator.java
@@ -8,8 +8,10 @@ import android.widget.Button;
 import android.widget.EditText;
 
 public class Calculator extends AppCompatActivity {
-    EditText editInput;
-    Button btn7, btn8, btn9, btnDivide, btn4, btn5, btn6, btnMultiplication, btn1, btn2, btn3,btnMinus, btn0, btnDot, btnPercent, btnPlus, btnAC, btnEqual;
+    EditText editInput, editResult;
+    Button btn7, btn8, btn9, btnDivision, btn4, btn5, btn6, btnMultiplication, btn1, btn2, btn3,btnSubstraction, btn0, btnDot, getBtnEqual, btnAddition, btnAC, btnBackspace, btnDel;
+    String currentValue, result;
+private boolean dot_inserted, operator_inserted;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,10 +19,18 @@ public class Calculator extends AppCompatActivity {
         setContentView(R.layout.activity_calculator);
 
         editInput=(EditText)findViewById(R.id.edtInput);
+        editResult=(EditText)findViewById(R.id.edtResult);
+
+        currentValue = "";
+        result = "";
+
+        dot_inserted = false;
+        operator_inserted = false;
+
         btn7=(Button) findViewById(R.id.btn7);
         btn8=(Button) findViewById(R.id.btn8);
         btn9=(Button) findViewById(R.id.btn9);
-        btnDivide=(Button) findViewById(R.id.btnDivide);
+        btnDivision=(Button) findViewById(R.id.btnDivision);
         btn4=(Button) findViewById(R.id.btn4);
         btn5=(Button) findViewById(R.id.btn5);
         btn6=(Button) findViewById(R.id.btn6);
@@ -28,142 +38,344 @@ public class Calculator extends AppCompatActivity {
         btn1=(Button) findViewById(R.id.btn1);
         btn2=(Button) findViewById(R.id.btn2);
         btn3=(Button) findViewById(R.id.btn3);
-        btnMinus=(Button) findViewById(R.id.btnMinus);
+        btnSubstraction=(Button) findViewById(R.id.btnSubstraction);
         btn0=(Button) findViewById(R.id.btn0);
         btnDot=(Button) findViewById(R.id.btnDot);
-        btnPercent=(Button) findViewById(R.id.btnPercent);
-        btnPlus=(Button) findViewById(R.id.btnPlus);
+        getBtnEqual=(Button) findViewById(R.id.btnEqual);
+        btnAddition=(Button) findViewById(R.id.btnAddition);
         btnAC=(Button) findViewById(R.id.btnAC);
-        btnEqual=(Button) findViewById(R.id.btnEqual);
+        btnBackspace=(Button) findViewById(R.id.btnBackspace);
+
 
 
         btn7.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                btn7.setText("7");
-                editInput.setText(btn7.getText().toString());
+                //btn7.setText("7");
+                //editInput.setText(editInput.getText().toString()+"7");
+                currentValue = currentValue + "7";
+                displayOne();
             }
         });
         btn8.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                btn8.setText("8");
-                editInput.setText(btn8.getText().toString());
+                //btn8.setText("8");
+                //editInput.setText(editInput.getText().toString()+"8");
+                currentValue = currentValue + "8";
+                displayOne();
             }
         });
         btn9.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                btn9.setText("9");
-                editInput.setText(btn9.getText().toString());
+                //btn9.setText("9");
+                //editInput.setText(editInput.getText().toString()+"9");
+                currentValue = currentValue + "9";
+                displayOne();
             }
         });
-        btnDivide.setOnClickListener(new View.OnClickListener() {
+        btnDivision.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                btnDivide.setText("/");
-                editInput.setText(btnDivide.getText().toString());
+                //btnDivide.setText("/");
+                //editInput.setText(editInput.getText().toString()+"/");
+
+                //set dot inserted = false
+                dot_inserted = false;
+
+                //check if current value is empty
+                if (!currentValue.isEmpty()){
+
+                }
+                    //check if last digit is dot => remove
+                    if (currentValue.substring(currentValue.length()-1, currentValue.length()).equals(".")){
+                        backspace();
+                    }
+                    //check if operator inserted == false => append operator to current string
+                    if (operator_inserted == false){
+                        currentValue = currentValue + " ÷ ";
+                        operator_inserted = true;
+                    }
+                displayOne();
             }
         });
         btn4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                btn4.setText("4");
-                editInput.setText(btn4.getText().toString());
+                //btn4.setText("4");
+                //editInput.setText(editInput.getText().toString()+"4");
+                currentValue = currentValue + "4";
+                displayOne();
             }
         });
         btn5.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                btn5.setText("5");
-                editInput.setText(btn5.getText().toString());
+                //btn5.setText("5");
+                //editInput.setText(editInput.getText().toString()+"5");
+                currentValue = currentValue + "5";
+                displayOne();
             }
         });
         btn6.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                btn6.setText("6");
-                editInput.setText(btn6.getText().toString());
+                //btn6.setText("6");
+                //editInput.setText(editInput.getText().toString()+"6");
+                currentValue = currentValue + "6";
+                displayOne();
             }
         });
+        /*btnMultiplication.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //btnMultiplication.setText("x");
+                //editInput.setText(editInput.getText().toString()+"x");
+                currentValue = currentValue + "x";
+                displayOne();
+            }
+        });*/
+
         btnMultiplication.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                btnMultiplication.setText("x");
-                editInput.setText(btnMultiplication.getText().toString());
+                //btnDivide.setText("/");
+                //editInput.setText(editInput.getText().toString()+"/");
+
+                //set dot inserted = false
+                dot_inserted = false;
+
+                //check if current value is empty
+                if (!currentValue.isEmpty()){
+
+                }
+                //check if last digit is dot => remove
+                if (currentValue.substring(currentValue.length()-1, currentValue.length()).equals(".")){
+                    backspace();
+                }
+                //check if operator inserted == false => append operator to current string
+                if (operator_inserted == false){
+                    currentValue = currentValue + " × ";
+                    operator_inserted = true;
+                }
+                displayOne();
             }
         });
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                btn1.setText("1");
-                editInput.setText(btn1.getText().toString());
+                //btn1.setText("1");
+                //editInput.setText(editInput.getText().toString()+"1");
+                currentValue = currentValue + "1";
+                displayOne();
             }
         });
         btn2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                btn2.setText("2");
-                editInput.setText(btn2.getText().toString());
+                //btn2.setText("2");
+                //editInput.setText(editInput.getText().toString()+"2");
+                currentValue = currentValue + "2";
+                displayOne();
             }
         });
         btn3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                btn3.setText("3");
-                editInput.setText(btn3.getText().toString());
+                //btn3.setText("3");
+                //editInput.setText(editInput.getText().toString()+"3");
+                currentValue = currentValue + "3";
+                displayOne();
             }
         });
-        btnMinus.setOnClickListener(new View.OnClickListener() {
+        /*btnSubstraction.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                btnMinus.setText("-");
-                editInput.setText(btnMinus.getText().toString());
+                //btnMinus.setText("-");
+                //editInput.setText(editInput.getText().toString()+"-");
+                currentValue = currentValue + "-";
+                displayOne();
+            }
+        });*/
+
+        btnSubstraction.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //btnDivide.setText("/");
+                //editInput.setText(editInput.getText().toString()+"/");
+
+                //set dot inserted = false
+                dot_inserted = false;
+
+                //check if current value is empty
+                if (!currentValue.isEmpty()){
+
+                }
+                //check if last digit is dot => remove
+                if (currentValue.substring(currentValue.length()-1, currentValue.length()).equals(".")){
+                    backspace();
+                }
+                //check if operator inserted == false => append operator to current string
+                if (operator_inserted == false){
+                    currentValue = currentValue + " - ";
+                    operator_inserted = true;
+                }
+                displayOne();
             }
         });
         btn0.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                btn0.setText("0");
-                editInput.setText(btn0.getText().toString());
+                //btn0.setText("0");
+                //editInput.setText(editInput.getText().toString()+"0");
+                currentValue = currentValue + "0";
+                displayOne();
             }
         });
         btnDot.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                btnDot.setText(".");
-                editInput.setText(btnDot.getText().toString());
+                //btnDot.setText(".");
+                //editInput.setText(editInput.getText().toString()+".");
+                //currentValue = currentValue + ".";
+                //displayOne();
+
+                //check if its empty => append "0." and set dot_inserted variable to true
+                if (currentValue.isEmpty()){
+                    currentValue = "0.";
+                    dot_inserted = true;
+                }
+
+
+                //check if dot_inserted == false => append "."
+                if (dot_inserted == false){
+                    currentValue = currentValue + ".";
+                    dot_inserted = true;
+                }
+                displayOne();
             }
         });
-        btnPercent.setOnClickListener(new View.OnClickListener() {
+        getBtnEqual.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                btnPercent.setText("%");
-                editInput.setText(btnPercent.getText().toString());
+                //btnPercent.setText("=");
+                //editInput.setText(editInput.getText().toString()+"%");
+                //currentValue = currentValue + "=";
+                //displayOne();
+
+                // check if the last digit is not an operator
+                if (operator_inserted==true && !currentValue.substring(currentValue.length()-1,currentValue.length()).equals(" ")){
+
+                    String [] tokens = currentValue.split(" ");
+
+                    switch (tokens[1].charAt(0)){
+
+                        case '÷':
+                            result = Double.toString(Double.parseDouble(tokens[0]) / Double.parseDouble(tokens[2]));
+                            break;
+                        case '×':
+                            result = Double.toString(Double.parseDouble(tokens[0]) * Double.parseDouble(tokens[2]));
+                            break;
+                        case '-':
+                            result = Double.toString(Double.parseDouble(tokens[0]) - Double.parseDouble(tokens[2]));
+                            break;
+                        case '+':
+                            result = Double.toString(Double.parseDouble(tokens[0]) + Double.parseDouble(tokens[2]));
+                            break;
+                    }
+                    displayTwo();
+                }
             }
         });
-        btnPlus.setOnClickListener(new View.OnClickListener() {
+        /*btnAddition.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                btnPlus.setText("+");
-                editInput.setText(btnPlus.getText().toString());
+                //btnPlus.setText("+");
+                //editInput.setText(editInput.getText().toString()+"+");
+                currentValue = currentValue + "+";
+                displayOne();
+            }
+        });*/
+
+        btnAddition.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //btnDivide.setText("/");
+                //editInput.setText(editInput.getText().toString()+"/");
+
+                //set dot inserted = false
+                dot_inserted = false;
+
+                //check if current value is empty
+                if (!currentValue.isEmpty()){
+
+                }
+                //check if last digit is dot => remove
+                if (currentValue.substring(currentValue.length()-1, currentValue.length()).equals(".")){
+                    backspace();
+                }
+                //check if operator inserted == false => append operator to current string
+                if (operator_inserted == false){
+                    currentValue = currentValue + " + ";
+                    operator_inserted = true;
+                }
+                displayOne();
             }
         });
         btnAC.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                btnAC.setText("AC");
-                editInput.setText(btnAC.getText().toString());
+                //btnAC.setText("AC");
+                //editInput.setText("");
+                clear();
+                displayOne();
+                displayTwo();
             }
         });
-        btnEqual.setOnClickListener(new View.OnClickListener() {
+        btnBackspace.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                btnEqual.setText("=");
-                editInput.setText(btnEqual.getText().toString());
+                //btnEqual.setText("=");
+                //editInput.setText(editInput.getText().toString()+"=");
+                //currentValue = currentValue + " ";                           //  baki ahe
+                backspace();
+                displayOne();
             }
         });
 
+
     }
+    public void displayOne(){                      //*
+            editInput.setText(currentValue);
+        }
+        public void displayTwo(){
+            editResult.setText(result);
+        }
+        public void clear(){
+        currentValue = "";
+        result = "";
+        dot_inserted = false;
+        operator_inserted = false;
+        }
+
+        public void backspace() {
+            //if current value is not empty => remove last character
+            if (!currentValue.isEmpty()) {
+
+                //check if the dot is last char in string => set dot_inserted = false
+                if (currentValue.substring(currentValue.length() - 1, currentValue.length()).equals(".")) {
+                    dot_inserted = false;
+                }
+
+                //if the operator is detected => delete three digits or chars from current val string and set operator_inserted = false
+                if (currentValue.substring(currentValue.length() - 1, currentValue.length()).equals(" ")) {
+                    currentValue = currentValue.substring(0, currentValue.length() - 3);
+                    operator_inserted = false;
+                } else {
+                    currentValue = currentValue.substring(0, currentValue.length() - 1);
+                }
+            }
+        }
 }
 
